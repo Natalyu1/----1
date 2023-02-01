c4ec022f4a50825d08c012996bf474ca53241cc4
Natalyu1 <bratchickova2010@yandex.ru>
2023-02-01 13:56:03 +0400

    запросы

--- src/app.js
+++ src/app.js
@@ -1,11 +1,15 @@
-async function searchTours() {
-    const respanse = await fetch('https://www.bit-by-bit.ru/api/student-projects/tours')
-    const tours = await respanse.json()
-       
+import { format } from "date-fns"
+import { ru } from "date-fns/locale"
 
-        tours.forEach(tour => {
-            document.getElementById("myContainer").innerHTML +=`
-            <div class="bg-white rounded-xl overflow-hidden">
+async function searchTours() {
+    const respanse = await fetch(
+        "https://www.bit-by-bit.ru/api/student-projects/tours"
+    )
+    const tours = await respanse.json()
+
+    tours.forEach((tour) => {
+        document.getElementById("myContainer").innerHTML += `
+            <div class="bg-white rounded-xl overflow-hidden flex flex-col justify-between">
                <div>
                 <img class="h-80 w-full" alt="изображение" src = "${tour.image}"/>
       
@@ -19,8 +23,7 @@ async function searchTours() {
                           <span class="font-bold mt-3 text-center text-lg">${tour.city}</span>
                           <span class="font-bold mt-3 text-center text-lg">${tour.hotelName}</span>
                           <p class="font-medium mt-1 v text-lg">${tour.price}</p>
-                          <span class="font-medium  text-gray-700 mt-3 text-lg">${tour.startTime}</span>
-                          <span class="font-medium  text-gray-700 mt-3 text-lg">${tour.endTime}</span>
+                          <p class="font-medium  text-gray-700 mt-3 text-lg">${format(new Date(tour.startTime), "dd MMMM yyyy", {locale: ru})} - ${format(new Date(tour.endTime), "dd MMMM yyyy", {locale: ru})}</p>
                           <p class="text-gray-500 mt-3">
                             Lorem ipsum dolor sit amet consectetur adipisicing elit.
                             Architecto accusantium praesentium eius, ut atque fuga culpa, 
@@ -31,25 +34,14 @@ async function searchTours() {
                     </div>
       
                     <div class="mt-3 text-gray-500 text-sm flex items-center ">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z" />
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z" />
-                          </svg>
-                          
-                        <span class="pl-1">7 km</span>
-                        <span class="mx-2" aria-hidden="true">&middot;</span>
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z" />
-                          </svg>
-                          
-                        <span class="pl-1">120 мин</span>
+
                     </div>
-                    <button class="button-color mt-4">Подробнее</button>
+                    <button class="button-color mt-4 pl-6">Подробнее</button>
                 </div>
+                 
                </div>
-      
-            
+               
             `
-       });
+    })
 }
 searchTours()
